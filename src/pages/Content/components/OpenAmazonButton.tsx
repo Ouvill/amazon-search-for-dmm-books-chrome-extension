72e73ff238eca07a90b8df8992bde71f1d65c24e
Ouvill <contact@ouvill.net>
2021-04-12 19:35:59 +0900

    change URL to book search

--- src/pages/Content/components/OpenAmazonButton.tsx
+++ src/pages/Content/components/OpenAmazonButton.tsx
@@ -5,7 +5,10 @@ const SearchAmazonButton: React.FC = () => {
   return (
     <>
       <span className={'m-boxDcFlatBtn m-boxDcFlatBtn--searchAmazon'}>
-        <a href={`https://www.amazon.co.jp/s?k=${getTitle()}`} target="_blank">
+        <a
+          href={`https://www.amazon.co.jp/s?k=${getTitle()}&i=stripbooks&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&`}
+          target="_blank"
+        >
           <span
             className={'m-boxDcFlatBtn__txt m-boxDcFlatBtn--searchAmazon__text'}
           >
